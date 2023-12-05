7db7c1b7b56f0db336da50a9cae7a74b01f3c4fa
KarakulakTulay <tuelay.karakulak@usz.ch>
2023-12-05 15:08:29 +0100

    easy secretsanta

--- main.py
+++ main.py
@@ -2,64 +2,45 @@
 # date: 05.12.2023
 
 import streamlit as st
-import smtplib
 import random
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
-import os
 
 # Streamlit UI
 st.title("Secret Santa App")
 
 with st.form("participant_form"):
     name = st.text_input("Name")
-    email = st.text_input("Email")
     submit_button = st.form_submit_button("Submit")
 
 if 'participants' not in st.session_state:
     st.session_state['participants'] = []
 
-if submit_button:
-    st.session_state['participants'].append({'name': name, 'email': email})
+if submit_button and name:
+    st.session_state['participants'].append(name)
 
 # Display participants
 st.write("Participants:")
 for participant in st.session_state['participants']:
-    st.write(participant['name'])
+    st.write(participant)
 
 # Secret Santa Logic
 def assign_secret_santa(participants):
     shuffled = participants[:]
     random.shuffle(shuffled)
+    assignments = {}
     for i in range(len(shuffled)):
         receiver_index = (i + 1) % len(shuffled)
-        shuffled[i]['receiver'] = shuffled[receiver_index]['name']
-    return shuffled
-
-# Email Sending Function
-def send_emails(assigned):
-    # Configure SMTP server
-    server = smtplib.SMTP('smtp.gmail.com', 587)
-    server.starttls()
-    server.login(os.getenv('EMAIL'), os.getenv('PASSWORD'))
-    
-    for participant in assigned:
-        msg = MIMEMultipart()
-        msg['From'] = os.getenv('EMAIL')
-        msg['To'] = participant['email']
-        msg['Subject'] = "Secret Santa Assignment"
-        body = f"You are the Secret Santa for {participant['receiver']}!"
-        msg.attach(MIMEText(body, 'plain'))
-        text = msg.as_string()
-        server.sendmail(os.getenv('EMAIL'), participant['email'], text)
-    server.quit()
+        assignments[shuffled[i]] = shuffled[receiver_index]
+    return assignments
 
 # Button to Trigger Assignment
 if st.button("Assign Secret Santas"):
-    assigned = assign_secret_santa(st.session_state['participants'])
-    send_emails(assigned)
-    st.session_state['participants'].clear()  # Clearing participants data
-    st.success("Secret Santa assigned and emails sent!")
+    if len(st.session_state['participants']) < 2:
+        st.error("Please add at least two participants.")
+    else:
+        assigned = assign_secret_santa(st.session_state['participants'])
+        st.subheader("Secret Santa Assignments:")
+        for giver, receiver in assigned.items():
+            st.write(f"{giver} 🎁 → {receiver}")
 
 # Clear Participants List
 if st.button("Clear Participants"):
